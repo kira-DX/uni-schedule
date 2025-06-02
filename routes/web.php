bb5d463a0ea188f10b7d24ab4ed41186e5cc74ce
kira-DX <kurakeyna@hotmail.com>
2025-06-02 20:35:37 +0900

    React修正

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,7 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
-// APIや他のルートがあれば上に定義
-
-// SPA用のcatch-allルート（最後に追加）
 Route::get('/{any}', function () {
-    $indexPath = public_path('react/index.html');
-    if (File::exists($indexPath)) {
-        return Response::make(file_get_contents($indexPath), 200)
-            ->header("Content-Type", "text/html");
-    } else {
-        abort(404, 'React index.html not found.');
-    }
+    $file = public_path('react/index.html');
+    return File::exists($file) ? Response::file($file) : abort(404);
 })->where('any', '.*');
