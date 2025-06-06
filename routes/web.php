1e19064da24f1dede3fdd44cec2000f575479f32
kira-DX <kurakeyna@hotmail.com>
2025-06-07 01:26:10 +0900

    LaravelとReactの共存

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    $file = public_path('react/index.html');
-    return File::exists($file) ? Response::file($file) : abort(404);
-})->where('any', '.*');
+Route::get('/', function () {
+    return view('welcome');
+});
