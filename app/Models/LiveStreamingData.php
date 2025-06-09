921fa554eed0dafe666b8393ce217b4167e40ee4
kira-DX <kurakeyna@hotmail.com>
2025-06-09 19:14:14 +0900

    新テーブルModel作成

--- app/Models/LiveStreamingData.php
+++ app/Models/LiveStreamingData.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class LiveStreamingData extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'channel_id',
+        'video_id',
+        'title',
+        'thumbnail_url',
+        'scheduled_time',
+        'live_status',
+        'type',
+    ];
+
+    public $timestamps = false;
+
+    protected $table = 'live_streaming_data';
+
+    protected $casts = [
+        'scheduled_time' => 'datetime',
+        'created_at' => 'datetime',
+    ];
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class, 'channel_id', 'channel_id');
+    }
 }
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    use HasFactory;
+    protected $primaryKey = 'channel_id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'channel_id',
+        'name',
+    ];
+
+    public $timestamps = false;
+
+    protected $table = 'members';
+
+    protected $casts = [
+        'created_at' => 'datetime',
+    ];
 }
