b9e672aa8c1093020ddaaeaad28cde6b5b137313
kira-DX <kurakeyna@hotmail.com>
2025-06-09 20:16:59 +0900

    Appをデフォルトextportに変更

--- resources/js/frontend/src/App.jsx
+++ resources/js/frontend/src/App.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MemberList from './components/MemberList';
 
+const App = () => <MemberList />;
+
+export default App;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MemberList />);
+root.render(<App />);
