2788bc392f8a07fe32c21a6406849c63d9deec9f
kira-DX <kurakeyna@hotmail.com>
2025-06-17 16:10:30 +0900

    開発用

--- resources/js/frontend-test/index.jsx
+++ resources/js/frontend-test/index.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
 import TestPage from './components/TestPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/test" element={<TestPage />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <TestPage />
+  </React.StrictMode>
 );
