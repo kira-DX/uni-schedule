bcace39d54b839bb20aa6b694b54f9b5da06fd25
kira-DX <kurakeyna@hotmail.com>
2025-06-17 16:30:46 +0900

    変更

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
-// / → React本体（既存のビルド済みSPA）
-Route::get('/', function () {
-    return file_get_contents(public_path('react/index.html'));
-});
-
 // /test → 別のReactビルド or 画面
 Route::get('/test', function () {
     return file_get_contents(public_path('react-test/index.html'));
+});
+
+// / → React本体（既存のビルド済みSPA）
+Route::get('/', function () {
+    return file_get_contents(public_path('react/index.html'));
 });
