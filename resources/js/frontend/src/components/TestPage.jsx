077eca027332b77785937fe85e3800215f7a176b
kira-DX <kurakeyna@hotmail.com>
2025-06-17 15:49:50 +0900

    テストページ作成

---
+++ resources/js/frontend/src/components/TestPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1>テストページ</h1>
+      <p>これはReactの /test ルートです。</p>
+    </div>
+  );
+};
+
+export default TestPage;
