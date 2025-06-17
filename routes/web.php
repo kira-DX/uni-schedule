34fd9ab0331a8452f2417e113acba9435740fcc1
kira-DX <kurakeyna@hotmail.com>
2025-06-17 15:46:15 +0900

    すべてのルートでReactのSPAを返す

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return file_get_contents(public_path('react/index.html'));
-});
-
-Route::get('/test', function () {
-    return file_get_contents(public_path('react-test/index.html'));
-});
+})->where('any', '.*');
